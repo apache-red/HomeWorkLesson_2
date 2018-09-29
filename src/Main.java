7d4da1c88ab89063e5f55891605ef311d053ddb0
apache-red <apache_red@mail.ru>
2018-09-29 20:42:28 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-    // Task_1 Console program
+    // Task_1 console program with input validation and loop structure
         OtherTask_1_IntervalConsoleProgram otherTask_1_Interval = new OtherTask_1_IntervalConsoleProgram();
         otherTask_1_Interval.start();
 
